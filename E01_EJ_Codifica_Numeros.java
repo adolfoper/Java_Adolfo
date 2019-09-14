fef0eca52caae4478a5535d2672d35cbe5b763ed
adolfoper <adolfoper@hotmail.com>
2019-09-14 03:51:23 +0200

    Update E01_EJ_Codifica_Numeros.java

--- E01_EJ_Codifica_Numeros.java
+++ E01_EJ_Codifica_Numeros.java
@@ -2,8 +2,6 @@ package ejercicios_T1;
 
 import java.util.Scanner;
 
-import ejercicios_T1.E01_EJ_Analiza_Texto.Par;
-
 import java.util.ArrayList;
 
 public class E01_EJ_Codifica_Numeros {
@@ -18,25 +16,32 @@ public class E01_EJ_Codifica_Numeros {
 									  		{'P','R','S'},
 									  		{'T','U','V'},
 									  		{'W','X','Y'}};
-	
+	 
 	public static void main (String args[]){
 		
 		String opcion = "";
 		
-/*		do
+		// Repetir hasta pulsar salir
+		do
 		{
-			System.out.println("\nIntroduzca un opción: 1-codificar, 2-decodificar)");	
-			opcion = sc.nextLine();
-		}
-		while (	!opcion.equals("1") & !opcion.equals("2"));
-		*/
-		opcion="1";
+			// Repetir hasta opción correcta
+			do
+			{
+				System.out.println("\nIntroduzca un opción: 1-Codificar, 2-Decodificar, 3-Finalizar)");	
+				opcion = sc.nextLine();
+				if (!opcion.equals("1") & !opcion.equals("2") & !opcion.equals("3"))
+					System.out.println("Opción errónea");	
+			}
+			while (	!opcion.equals("1") & !opcion.equals("2") & !opcion.equals("3"));
 		
-		if (opcion.equals("1"))
-			codifica_A_Numero();
- 		else
- 			decodifica_A_Palabra();
+			if (opcion.equals("1"))
+				codifica_A_Numero();
+			else if (opcion.equals("2"))
+				decodifica_A_Palabra();
+			
+		} while (!opcion.equals("3"));
 		
+		System.out.println("Fin del programa");	
 		sc.close(); 
 	}
 	
@@ -44,7 +49,7 @@ public class E01_EJ_Codifica_Numeros {
 	// Converte una palabra a numéro codificado
 	public static void codifica_A_Numero (){
 				
-		System.out.println("\ncarácteres no reconocibles se mostrarán como ceros");	
+		System.out.println("\nCarácteres no reconocibles se mostrarán como ceros");	
 		System.out.println("\nIntroduzca una palabra y pulse intro");	
   		String palabra = sc.nextLine();
   		
@@ -56,30 +61,8 @@ public class E01_EJ_Codifica_Numeros {
   			cod_numero += obtiene_Cod_Numero(c);
   		}	
 		System.out.println("Resultado: "+cod_numero);	
-  		
 	}
 	
-	// Converte un numéro codificado a las posibles palabras decodificadas
-	public static void decodifica_A_Palabra (){
-				
-		ArrayList<Pos_Itera> lista_posiciones = new ArrayList<Pos_Itera>();
-		
-		System.out.println("\ndígitos no reconocibles se mostrarán como espacios");	
-		System.out.println("\nIntroduzca un número con dígitos de 2 a 9 y pulse intro");	
-		String numero = sc.nextLine();
-
-  		// Recorre el número asignandole una posicion en la tabla de conversion e
-		// inicializando el número de interaciones a 0
-		for (char c: numero.toCharArray ()) 
-  		{
-			lista_posiciones.add(obtiene_Posicion(c));
-  		}
-	
-		// Genera la lista de palabras codificadas
-		ArrayList<String> palabras_cod = genera_Palabras_Cod(lista_posiciones);
-		
-	}
-
 	// Encuentra la codificación de un caracter a numérico
 	// Devuelve "0" si no lo encuentra
 	public static String obtiene_Cod_Numero (char c)
@@ -104,66 +87,84 @@ public class E01_EJ_Codifica_Numeros {
 		return encontrado;
 	}
 	
-	// Asigna a un caracter numérico una posicion en la tabla de conversion e
-	// inicializa su número de interaciones a 0
-	public static Pos_Itera obtiene_Posicion (char c)
-	{
+	// Converte un numéro codificado a las posibles palabras decodificadas
+	public static void decodifica_A_Palabra (){
+				
+		ArrayList<Integer> lista_posiciones = new ArrayList<Integer>();
+		ArrayList<String> lista_palabras_cod = new ArrayList<String>();
+		
+		System.out.println("\nDígitos no reconocibles se mostrarán como espacios");	
+		System.out.println("\nIntroduzca un número con dígitos de 2 a 9 y pulse intro");	
+		String numero = sc.nextLine();
+
+  		// Recorre el número asignandole una posicion en la tabla de conversion e
+		// inicializando el número de interaciones a 0
+		for (char c: numero.toCharArray ()) 
+  		{
+			lista_posiciones.add(obtiene_Posicion(c));
+  		}
+		
+		// Genera la lista de palabras codificadas
+		lista_palabras_cod = genera_Palabras_Cod(lista_palabras_cod, lista_posiciones,0,"");
+		
+		System.out.println("\nPalabras posibles:");	
+
+		for (String s :lista_palabras_cod)
+			System.out.println(s);
+	}
 	
-	  	Pos_Itera pos_itera = new Pos_Itera(-1,0);
-	  	
+	// Asigna a un caracter numérico su posicion en la tabla de conversion
+	public static int obtiene_Posicion (char c)
+	{ 	
+		Integer pos;
+		
 		if (c>='2' & c<='9')
 		{
-			pos_itera.posicion = (Integer.valueOf(c)-2);
+			pos = c - '0';
+			pos = pos - 2;
 		}
+		else
+			pos = -1;
 		
-		return pos_itera;
+		return pos;
 	}
 	
 	// Devuelve una lista de palabras generada a partir de una lista de posiciones de 
 	// la tabla de conversion.
-	public static ArrayList<String> genera_Palabras_Cod (ArrayList<Pos_Itera> lista_posiciones)
+	// Es recursiva.
+	// Variables: Lista donde se cargan las palabras, lista de posiciones de letras, posicion actual en la lista de posiciones de letras, palabra actual 
+	public static ArrayList<String> genera_Palabras_Cod (ArrayList<String> lista_palabras_cod, ArrayList<Integer> lista_posiciones, int indice, String palabra_ent)
 	{	
-		ArrayList<String> lista_palabras_cod = new ArrayList<String>();
+		String letra;
+		String palabra;
 		
-		// compone las palabras a partir de la tabla de conversion y las añade a la lista de salida
-		for (int i=0; i<lista_posiciones.size(); i++)
+		// posicion actual
+		int fila = lista_posiciones.get(indice);
+		
+		// para las tres columnas de la posicion
+		for (int i=0; i<3; i++)
 		{
-			String palabra = "";
-			int pos = lista_posiciones.get(i).posicion;
-					
-			for (int j=0; j<3; j++)
-			{
-				palabra += Character.toString(tabla_conversion[i][j]);
-			}
-			lista_palabras_cod.add(palabra);
+			// recupera la letra que corresponde a la posición / columna actual
+			if (fila == -1)
+				letra = " ";
+			else
+				letra = Character.toString(tabla_conversion[fila] [i]);
+
+			// incorpora letra a la palabra
+			if (indice == 0)
+				palabra = letra;
+			else
+				palabra = palabra_ent + letra;
+			
+			// Si no hemos llegado al final de la lista llamamos al siguiente nivel 
+			if (indice < lista_posiciones.size()-1)
+				lista_palabras_cod = genera_Palabras_Cod (lista_palabras_cod, lista_posiciones, indice+1, palabra);
+			else
+			// si nos hallamos al final de la lista. Incorporamos la palabra a la lista resultado.
+				lista_palabras_cod.add(palabra);
 		}
 		
 		return lista_palabras_cod;
 	}
 	
-	//***************************************************
-	// clase de datos posicion + iteracion
-	//***************************************************
-	public static class Pos_Itera
-	{
-		//creamos los atributos privados
-		public int posicion;
-		public int iteracion;
-		
-	//
-	//   Constructor sin parametros
-	//
-		public Pos_Itera() {
-			this.posicion =0;
-			this.iteracion =0;
-		}
-	
-	//
-	//	Constructor con parametros
-	//
-		public Pos_Itera(int posicion, int iteracion) {
-			this.posicion  =posicion;
-			this.iteracion =iteracion;
-		}
-	}
 }
